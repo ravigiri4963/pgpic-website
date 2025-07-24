e9fae01b1e8f2d094ece6380509b17ed08b5ddcc
unknown <ravigiri4963@gmail.com>
2025-07-24 09:36:30 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add a smooth scroll or other minor JS interactivity
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    })
+  })
+})
